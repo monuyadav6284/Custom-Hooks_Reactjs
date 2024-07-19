f023ba0c97e647032c530b8987279ef6b85c2be5
monuyadav6284 <monu628493@gmail.com>
2024-07-19 18:27:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  const [state, setstate] = useState();
+  useEffect(() => {
+    getAlldata();
+  }, []);
+
+  const getAlldata = async () => {
+    try {
+      const product = await fetch("https://fakestoreapi.com/products");
+      const productjson = await product.json(); // Wait for the promise to resolve
+      setstate(productjson);
+    } catch (error) {
+      console.log(error);
+    }
+     
+    
+  };
+
+  console.log("data from api", state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1> Welcome to our website!</h1>
+      {state &&
+        state.map((product) => (
+          <div key={product.id}>
+            <h2>{product.price}</h2>
+            <img src={product.image} alt={product.name} />
+            
+          </div>
+        ))}
+    </>
   );
 }
 
